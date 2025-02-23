b229dcf921a6bc2f6f42ed7edc63c7895deb5e1e
okezie okechukwu <samuelokezie.okechukwu@tiekinetix.com>
2025-02-23 23:46:54 +0000

    modify entity factory

--- pet-clinic-utils/src/main/java/com/samuel/sniffers/api/factory/EntityFactory.java
+++ pet-clinic-utils/src/main/java/com/samuel/sniffers/api/factory/EntityFactory.java
@@ -1,14 +1,14 @@
 package com.samuel.sniffers.api.factory;
 
-import com.samuel.sniffers.api.exception.ConversionException;
+import com.samuel.sniffers.api.exception.EntityMappingException;
 
 import java.util.List;
 
 /**
- * This interface provides conversion utilities using Jackson's ObjectMapper.
+ * This interface provides utility methods for entity mapping using Jackson's ObjectMapper.
  * Implementations of this interface will handle data transformations and serialization.
  */
-public interface ModelFactory {
+public interface EntityFactory {
 
     /**
      * Converts an entity to a Data Transfer Object (DTO).
@@ -18,7 +18,7 @@ public interface ModelFactory {
      * @param entity The entity to be converted.
      * @param pojoClass The target DTO class.
      * @return The converted DTO.
-     * @throws ConversionException if the conversion fails.
+     * @throws EntityMappingException if mapping fails.
      */
     <D, P> P convertToDTO(D entity, Class<P> pojoClass);
 
@@ -30,9 +30,21 @@ public interface ModelFactory {
      * @param pojo The DTO to be converted.
      * @param entityClass The target entity class.
      * @return The converted entity.
-     * @throws ConversionException if the conversion fails.
+     * @throws EntityMappingException if mapping fails.
      */
     <D, P> D convertToEntity(P pojo, Class<D> entityClass);
+
+    /**
+     * Updates entity with non-null fields from pojo object.
+     *
+     * @param <D> The type of the entity.
+     * @param <P> The type of the DTO.
+     * @param pojo The DTO with fields for updating entity.
+     * @param entity The entity class to be updated.
+     * @return The updated entity.
+     * @throws EntityMappingException if mapping fails.
+     */
+    public <D, P> D patchEntity(P pojo, D entity);
 
     /**
      * Converts a list of entities to a list of Data Transfer Objects (DTOs).
@@ -42,7 +54,7 @@ public interface ModelFactory {
      * @param entities The list of entities to be converted.
      * @param pojoClass The target DTO class.
      * @return The list of converted DTOs.
-     * @throws ConversionException if any conversion in the list fails.
+     * @throws EntityMappingException if any mapping in the list fails.
      */
     <D, P> List<P> convertToDTOList(List<D> entities, Class<P> pojoClass);
 
@@ -54,7 +66,7 @@ public interface ModelFactory {
      * @param pojos The list of DTOs to be converted.
      * @param entityClass The target entity class.
      * @return The list of converted entities.
-     * @throws ConversionException if any conversion in the list fails.
+     * @throws EntityMappingException if any mapping in the list fails.
      */
     <D, P> List<D> convertToEntityList(List<P> pojos, Class<D> entityClass);
 
@@ -65,7 +77,7 @@ public interface ModelFactory {
      * @param json The JSON string to be converted.
      * @param targetType The class type of the object.
      * @return The converted object.
-     * @throws ConversionException if the conversion fails.
+     * @throws EntityMappingException if mapping fails.
      */
     <T> T convertFromString(String json, Class<T> targetType);
 
@@ -76,7 +88,7 @@ public interface ModelFactory {
      * @param json The JSON string to be converted.
      * @param targetClass The class type of the objects in the list.
      * @return The list of converted objects.
-     * @throws ConversionException if the conversion fails.
+     * @throws EntityMappingException if mapping fails.
      */
     <T> List<T> convertFromStringToList(String json, Class<T> targetClass);
 
@@ -86,7 +98,7 @@ public interface ModelFactory {
      * @param <T> The type of the object.
      * @param object The object to be converted.
      * @return The JSON string representation of the object.
-     * @throws ConversionException if the conversion fails.
+     * @throws EntityMappingException if mapping fails.
      */
     <T> String convertToString(T object);
 
@@ -96,7 +108,7 @@ public interface ModelFactory {
      * @param <T> The type of the entities.
      * @param entityList The list of entities to be converted.
      * @return The JSON string representation of the list of entities.
-     * @throws ConversionException if the conversion fails.
+     * @throws EntityMappingException if mapping fails.
      */
     <T> String convertEntityListToString(List<T> entityList);
 
