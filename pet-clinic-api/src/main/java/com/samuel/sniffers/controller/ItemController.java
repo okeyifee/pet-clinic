105825c027e8e2450af84cfa70de654b7291c9e2
okezie okechukwu <samuelokezie.okechukwu@tiekinetix.com>
2025-02-23 23:49:49 +0000

    add controller endpoints

---
+++ pet-clinic-api/src/main/java/com/samuel/sniffers/controller/ItemController.java
@@ -0,0 +1,119 @@
+package com.samuel.sniffers.controller;
+
+import com.samuel.sniffers.api.response.ApiResponse;
+import com.samuel.sniffers.dto.BatchItemUpdateDTO;
+import com.samuel.sniffers.dto.ItemDTO;
+import com.samuel.sniffers.dto.UpdateItemDTO;
+import com.samuel.sniffers.dto.response.ItemBatchUpdateResponseDTO;
+import com.samuel.sniffers.dto.response.ItemResponseDTO;
+import com.samuel.sniffers.service.ItemService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+/*
+  This class provides CRUD methods for items.
+*/
+@RestController
+@RequestMapping(value = "/v1/customer/{customerId}/basket/{basketId}/item", produces = MediaType.APPLICATION_JSON_VALUE)
+@Tag(name = "Item controller", description = "APIs for managing basket items")
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @Operation(summary = "Create basket item", description = "Creates an item in a basket")
+    @PostMapping
+    public ResponseEntity<ApiResponse<ItemResponseDTO>> createItem(
+            @PathVariable String customerId,
+            @PathVariable String basketId,
+            @Valid @RequestBody ItemDTO dto) {
+        ItemResponseDTO createdItem = itemService.createItem(customerId, basketId, dto);
+        return ResponseEntity
+                .created(URI.create(String.format("/api/v1/customer/%s/basket/%s/item/%s", customerId, basketId, createdItem.getId())))
+                .body(ApiResponse.created("Item created successfully.", createdItem));
+    }
+
+    @Operation(summary = "Retrieve an item", description = "Fetches the details of an item using id.")
+    @GetMapping("/{itemId}")
+    public ResponseEntity<ApiResponse<ItemResponseDTO>> getItem(
+            @PathVariable String customerId,
+            @PathVariable String basketId,
+            @PathVariable String itemId) {
+        return ResponseEntity.ok(
+                ApiResponse.success("Basket retrieved successfully.",
+                        itemService.getItem(customerId, basketId, itemId)
+                )
+        );
+    }
+
+    @Operation(summary = "Retrieve all items", description = "Retrieves all items in a given basket for a given customer")
+    @GetMapping
+    public ResponseEntity<ApiResponse<List<ItemResponseDTO>>> getItems(
+            @PathVariable String customerId,
+            @PathVariable String basketId
+    ) {
+        List<ItemResponseDTO> items = itemService.getAllItems(customerId, basketId);
+        return ResponseEntity.ok(ApiResponse.success("Items retrieved successfully.", items));
+    }
+
+    @Operation(summary = "Update an item", description = "Updates the given item")
+    @PatchMapping("/{itemId}")
+    public ResponseEntity<ApiResponse<ItemResponseDTO>> updateItem(
+            @PathVariable String customerId,
+            @PathVariable String basketId,
+            @PathVariable String itemId,
+            @Valid @RequestBody UpdateItemDTO dto) {
+        ItemResponseDTO updatedItem = itemService.updateItem(customerId, basketId, itemId, dto);
+        return ResponseEntity.ok(ApiResponse.success("Item updated successfully.", updatedItem));
+    }
+
+    @Operation(summary = "Update item", description = "Update item in a basket")
+    @PutMapping("/{basketId}")
+    public ResponseEntity<ApiResponse<ItemResponseDTO>> updateItem(
+            @PathVariable String customerId,
+            @PathVariable String basketId,
+            @PathVariable String itemId,
+            @Valid @RequestBody ItemDTO dto) {
+        ItemResponseDTO updatedItem = itemService.updateItem(customerId, basketId, itemId, dto);
+        return ResponseEntity.ok(ApiResponse.success("Item updated successfully.", updatedItem));
+    }
+
+    @Operation(summary = "Update items in batch", description = "Batch operation to update items in a basket")
+    @PatchMapping
+    public ResponseEntity<ApiResponse<ItemBatchUpdateResponseDTO>> batchUpdateItems(
+            @PathVariable String customerId,
+            @PathVariable String basketId,
+            @Valid @RequestBody BatchItemUpdateDTO dto) {
+        ItemBatchUpdateResponseDTO updatedItems = itemService.batchUpdateItems(customerId, basketId, dto);
+        return ResponseEntity.ok(ApiResponse.success("Items updated successfully.", updatedItems));
+    }
+
+    @Operation(summary = "Check if an item exists", description = "Checks if an item exists by Id")
+    @RequestMapping(value = "/{itemId}", method = RequestMethod.HEAD)
+    public ResponseEntity<Void> checkItemExists(
+            @PathVariable String customerId,
+            @PathVariable String basketId,
+            @PathVariable String itemId) {
+        itemService.getItem(customerId, basketId, itemId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @Operation(summary = "Delete an item", description = "Deletes an item from a basket")
+    @DeleteMapping("/{itemId}")
+    public ResponseEntity<ApiResponse<Void>> deleteItem(
+            @PathVariable String customerId,
+            @PathVariable String basketId,
+            @PathVariable String itemId
+    ) {
+        itemService.deleteItem(customerId, basketId, itemId);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ pet-clinic-api/src/main/java/com/samuel/sniffers/controller/ShoppingBasketController.java
@@ -0,0 +1,89 @@
+package com.samuel.sniffers.controller;
+
+import com.samuel.sniffers.api.response.ApiResponse;
+import com.samuel.sniffers.dto.BatchBasketUpdateDTO;
+import com.samuel.sniffers.dto.UpdateBasketDTO;
+import com.samuel.sniffers.dto.response.BasketBatchUpdateResponseDTO;
+import com.samuel.sniffers.dto.response.BasketResponseDTO;
+import com.samuel.sniffers.service.ShoppingBasketService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+/*
+  This class provides CRUD methods for basket.
+*/
+@RestController
+@RequestMapping(value = "/v1/customer/{customerId}/basket", produces = MediaType.APPLICATION_JSON_VALUE)
+@Tag(name = "Basket controller", description = "APIs for managing customers basket")
+@RequiredArgsConstructor
+public class ShoppingBasketController {
+
+    private final ShoppingBasketService basketService;
+
+    @Operation(summary = "Create shopping basket", description = "Creates a new shopping basket for the customer")
+    @PostMapping
+    public ResponseEntity<ApiResponse<BasketResponseDTO>> createBasket(@PathVariable String customerId) {
+        BasketResponseDTO basket = basketService.createBasket(customerId);
+        return ResponseEntity
+                .created(URI.create(String.format("/api/v1/customer/%s/basket/%s", customerId, basket.getId())))
+                .body(ApiResponse.created("Shopping basket created successfully.", basket));
+    }
+
+    @Operation(summary = "Retrieve a basket", description = "Fetches the details of a shopping basket by its ID")
+    @GetMapping("/{basketId}")
+    public ResponseEntity<ApiResponse<BasketResponseDTO>> getBasket(@PathVariable String customerId, @PathVariable String basketId) {
+        return ResponseEntity.ok(
+                ApiResponse.success("Basket retrieved successfully.",
+                        basketService.getBasket(customerId, basketId)
+                )
+        );
+    }
+
+    @Operation(summary = "Retrieve all baskets for a customer", description = "Fetches all shopping baskets belonging to a customer")
+    @GetMapping
+    public ResponseEntity<ApiResponse<List<BasketResponseDTO>>> getCustomerBaskets(@PathVariable String customerId) {
+        List<BasketResponseDTO> baskets = basketService.getAllBaskets(customerId);
+        return ResponseEntity.ok(ApiResponse.success("Baskets retrieved successfully.", baskets));
+    }
+
+    @Operation(summary = "Update shopping basket", description = "Updates the shopping basket")
+    @PatchMapping("/{basketId}")
+    public ResponseEntity<ApiResponse<BasketResponseDTO>> updateBasket(
+            @PathVariable String customerId,
+            @PathVariable String basketId,
+            @Valid @RequestBody UpdateBasketDTO dto) {
+        BasketResponseDTO updatedBasket = basketService.updateBasket(customerId, basketId, dto);
+        return ResponseEntity.ok(ApiResponse.success("Basket updated successfully.", updatedBasket));
+    }
+
+    @Operation(summary = "Update shopping baskets in batch", description = "Batch operation to update shopping baskets")
+    @PatchMapping
+    public ResponseEntity<ApiResponse<BasketBatchUpdateResponseDTO>> batchUpdateBasket(
+            @PathVariable String customerId,
+            @Valid @RequestBody BatchBasketUpdateDTO dto) {
+        BasketBatchUpdateResponseDTO updatedBasket = basketService.batchUpdateBasket(customerId, dto);
+        return ResponseEntity.ok(ApiResponse.success("Basket updated successfully.", updatedBasket));
+    }
+
+    @Operation(summary = "Check if a basket exists", description = "Checks if a shopping basket exists by its ID")
+    @RequestMapping(value = "/{basketId}", method = RequestMethod.HEAD)
+    public ResponseEntity<Void> checkBasketExists(@PathVariable String customerId, @PathVariable String basketId) {
+        basketService.getBasket(customerId, basketId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @Operation(summary = "Delete a shopping basket", description = "Deletes a shopping basket by its ID")
+    @DeleteMapping("/{basketId}")
+    public ResponseEntity<ApiResponse<Void>> deleteBasket(@PathVariable String customerId, @PathVariable String basketId) {
+        basketService.deleteBasket(customerId, basketId);
+        return ResponseEntity.noContent().build();
+    }
+}
